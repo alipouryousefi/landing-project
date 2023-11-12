82c7e6ac6d8e820c554f136cb19cdd67a91921b3
Ali Py <a.pouryousefi98@gmail.com>
2023-11-12 09:56:51 +0330

    test full screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,32 @@
 import React, { useRef } from 'react';
 
 const FullScreenComponent = () => {
-  const containerRef = useRef(null);
+  const containerRef:any = useRef(null);
 
   const requestFullScreen = () => {
     const container:any = containerRef.current;
 
     if (container) {
+      // Check if the Fullscreen API is supported
       if (container.requestFullscreen) {
         container.requestFullscreen();
       } else if (container.webkitRequestFullscreen) {
+        // For Safari on iOS, use a user-triggered event to request fullscreen
+        container.addEventListener('click', handleSafariFullScreenRequest);
         container.webkitRequestFullscreen();
       } else if (container.msRequestFullscreen) {
         container.msRequestFullscreen();
       }
+    }
+  };
+
+  // Event handler for Safari on iOS
+  const handleSafariFullScreenRequest = () => {
+    const container:any = containerRef.current;
+
+    if (container) {
+      container.removeEventListener('click', handleSafariFullScreenRequest);
+      container.webkitRequestFullscreen();
     }
   };
 
